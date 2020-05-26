88bca20fa7adad0c405fc4fc23ebb4ad8742d076
aryanarora180 <aryan.arora180@gmail.com>
2020-05-26 15:52:34 +0530

    ActionBar title changes according to Fragment

--- app/src/main/java/com/example/expensify/MainActivity.kt
+++ app/src/main/java/com/example/expensify/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.expensify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.expensify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
 }
